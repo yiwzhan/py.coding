0b4d3986b669759667b7c604a50eaa9ba87ad248
itsbryan365 <itsbryan365@gmail.com>
2020-07-02 11:00:10 -0700

    s

--- python4kids/c3.py
+++ python4kids/c3.py
@@ -1,5 +1,5 @@
 #hi=(12,3232323,23,2322)
 #print(hi[3])
 true={'andrew':'dumb','bryan':'smart'}
-print('andrew is',favr['andrew'])
-print('bryan is',favr['bryan'])
+print('andrew is',true['andrew'])
+print('bryan is',true['bryan'])
